505625a29c9ec9ecaff03fbefcdbd5b74883b437
Kelly James Clancy <kellyjamesclancy@gmail.com>
2016-11-04 19:26:49 +0000

    Private function added

--- mod_ex.js
+++ mod_ex.js
@@ -1,6 +1,11 @@
+function private() {
+  return 'private';
+};
+
 module.exports = {
   pigeons: Math.floor(Math.random() * 100),
   explain: function() {
     console.log('It is sunny in Vancouver, and  there are ' + this.pigeons + ' very happy pigeons today!');
+    console.log('Here is evidence of a ' + private() + ' function.');
   }
 };
